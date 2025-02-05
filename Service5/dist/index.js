3db0e5a49a2f3281ac66f515fe937751edad8098
AzureAD\SahilSaini <sahil.saini@manovapartners.com>
2025-02-05 16:42:39 +0100

    nginx initial setup added

--- Service5/dist/index.js
+++ Service5/dist/index.js
@@ -8,16 +8,16 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const cors_1 = __importDefault(require("cors")); // Use import instead of require
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-const port = process.env.PORT || 8500;
+const port = process.env.PORT || 8600;
 // CORS setup
 app.use((0, cors_1.default)({ origin: '*' }));
 app.get("/", (req, res) => {
-    res.send("Service 4");
+    res.send("Service 5");
 });
-app.get("/service4check", (req, res) => {
-    res.json({ "message": "Service 4 is up" });
+app.get("/service5check", (req, res) => {
+    res.json({ "message": "Service 5 is up" });
 });
-app.get("/service4port", (req, res) => {
+app.get("/service5port", (req, res) => {
     res.send(`${port}`);
 });
 app.listen(port, () => {
--- Service5/src/index.ts
+++ Service5/src/index.ts
@@ -5,20 +5,20 @@ import cors from "cors"; // Use import instead of require
 dotenv.config();
 
 const app: Express = express();
-const port = process.env.PORT || 8500;
+const port = process.env.PORT || 8600;
 
 // CORS setup
 app.use(cors({ origin: '*' }));
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Service 4");
+  res.send("Service 5");
 });
 
-app.get("/service4check", (req: Request, res: Response) => {  
-  res.json({ "message": "Service 4 is up" });
+app.get("/service5check", (req: Request, res: Response) => {  
+  res.json({ "message": "Service 5 is up" });
 });
 
-app.get("/service4port", (req: Request, res: Response) => {
+app.get("/service5port", (req: Request, res: Response) => {
   res.send(`${port}`);
 });
 
